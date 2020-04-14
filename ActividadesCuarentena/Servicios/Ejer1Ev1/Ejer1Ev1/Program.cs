fffa91cfd1c4f6bbe9799afa83078f0dbfdf3fd0
pelea08 <pelea08@users.noreply.github.com>
2020-04-14 23:30:04 +0200

    Mejora General V2

--- ActividadesCuarentena/Servicios/Ejer1Ev1/Ejer1Ev1/Program.cs
+++ ActividadesCuarentena/Servicios/Ejer1Ev1/Ejer1Ev1/Program.cs
@@ -11,61 +11,139 @@ namespace Ejer1Ev1
     {
         static bool finalizacion = false;
         static readonly object l = new object();
+        static int ganador;
+        static Timer timer;
+        static int apuesta;
+
         static void Main(string[] args)
         {
+            Thread tropezar;
+
+            int repetir = 1;
 
             Random r = new Random();
             int diez = r.Next(1, 10);
             int distancia = diez;
             int tiempo = diez;
 
-            Thread[] participantes = new Thread[5];
-
-            for (int i = 0; i < 5; i++)
+            while (repetir == 1)
             {
-                participantes[i] = new Thread(caballos);
-            }
-            for (int j = 0; j < participantes.Length; j++)
-            {
-                participantes[j].Start(j);
-            }
+                //Console.Clear();
+                finalizacion = false;
+                repetir = 2;
+                do
+                {
+                    try
+                    {
+
+                        Console.WriteLine("Apueste por un caballo del 1 al 5");
+                        apuesta = Convert.ToInt32(Console.ReadLine());
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Introduzca números no letras");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Introduzca un número valido entre el 1 y el 5");
+                    }
+                } while (apuesta < 1 || apuesta > 5);
+
+                Console.Clear();
 
 
+                Thread[] conjuntoCaballos = new Thread[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    conjuntoCaballos[i] = new Thread(caballos);
+                }
+                for (int j = 0; j < conjuntoCaballos.Length; j++)
+                {
+                    conjuntoCaballos[j].Start(j);
+                }
 
-        }
-        static void caballos(object caballo)
-        {
-            int i = 0;
-
-            while (!finalizacion)
-            {
                 lock (l)
                 {
-                    Console.SetCursorPosition(1+i, (int)caballo + 1);
-                    Console.Write("*");
-                    i++;
-
-                    if (i == 30)
+                    Monitor.Wait(l);
+                    if (apuesta == ganador)
                     {
-                        finalizacion = true;
+                        Console.SetCursorPosition(1, 23);
+                        Console.Write("ENHORABUENA GANASTE\n");
                     }
+                    else
+                    {
+                        Console.SetCursorPosition(1, 23);
+                        Console.Write("HAS PERDIDO\n");
+                    }
+                    bool band = false;
+                    do
+                    {
+                        try
+                        {
+                            //Console.SetCursorPosition(1, 25);
+                            Console.WriteLine("Desea volver a jugar 1-Si 2-No");
+                            repetir = Convert.ToInt32(Console.ReadLine());
+                            band = false;
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Introduzca números no letras");
+                            band = true;
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Introduzca un número valido entre el 1 y el 5");
+                            band = true;
+                        }
 
-
-
+                    } while (repetir < 1 || repetir > 2 || band);
 
                 }
 
 
 
+            }
+            Console.ReadLine();
+
+        }
 
 
 
 
+        static void caballos(object caballo)
+        {
+            Random r = new Random();
+            int dormirAleatorio = r.Next(200, 1000);
+            int distanciaAleatoria = r.Next(1, 20);
+            int i = 1;
 
+            while (!finalizacion)
+            {
+                int caballoActual = (int)caballo + 1;
+                lock (l)
+                {
+                    if (!finalizacion)
+                    {
+                        Console.SetCursorPosition(1 + i, caballoActual);
+                        Console.Write("*");
+                        i++;
+
+                        if (i == 5)
+                        {
+                            finalizacion = true;
+                            ganador = (int)caballo;
+                            Monitor.Pulse(l);
+                            //intersante al llegar aqui implica finalizacion es decir plantearse pulse 
+
+                        }
+                    }
+
+                }
+                //ojo poner variable aleatorio
+                Thread.Sleep(125);
             }
 
-
-            Console.ReadLine();
         }
     }
 }
---
+++ ActividadesCuarentena/Servicios/Ejer2Ev1/Ejer2Ev1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer2Ev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("1.-Visualizar todos los procesos");
+                    Console.WriteLine("2.-Información de un proceso");
+                    Console.WriteLine("3.-Cierre de un proceso");
+                    Console.WriteLine("4.-Cierre forzoso de un proceso");
+                    Console.WriteLine("5.-Ejecucion de una aplicacion");
+                    Console.WriteLine("6.-Guardar en archivo");
+                    Console.WriteLine("7.-Leer archivo");
+                    Console.WriteLine("8.-Salir");
+                    op = Convert.ToInt32(Console.ReadLine());
+
+                    switch (op)
+                    {
+
+                        case 1:
+                            break;
+
+
+
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Introduzca un número valido");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Introduzca números");
+                }
+
+
+            } while (op < 1 || op > 8);
+
+
+        }
+    }
+}
