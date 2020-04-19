31368837def310ee05a33e2c15d9636f033f6e18
pelea08 <pelea08@users.noreply.github.com>
2020-04-20 01:14:05 +0200

    Actulización Ejer 2

--- ActividadesCuarentena/DI/Ejer2Ev1/Ejer2Ev1/Program.cs
+++ ActividadesCuarentena/DI/Ejer2Ev1/Ejer2Ev1/Program.cs
@@ -8,6 +8,13 @@ namespace Ejer2Ev1
 {
     class Program
     {
+        public enum asignauras
+        {
+            facebookads,
+            marketing,
+            seo,
+            sem
+        }
         static void Main(string[] args)
         {
             string[] nombres = { "Pedro", "Jesus", "Maria", "Jose", "Peter", "Mariano", "Pablo", "Tesla", "Fatema", "Jesusito", "Manolito", "Santiago" };
@@ -15,8 +22,8 @@ namespace Ejer2Ev1
             Random r = new Random();
             //4 asignaturas 12 alumnos
             int[,] tabla = new int[4, 12];
-            int i = 0;
-            int j = 0;
+            int i;
+            int j;
             for (i = 0; i < tabla.GetLength(0); i++)
             {
                 for (j = 0; j < tabla.GetLength(1); j++)
@@ -86,33 +93,146 @@ namespace Ejer2Ev1
 
                 switch (op)
                 {
+                    //Vigilar Excepciones00000000000000000000000000000000000000000000000000
                     case 1:
-                        int suma = 0;
-                        int res ;
+                        double suma = 0;
+                        double res;
                         for (int k = 0; k < tabla.GetLength(0); k++)
                         {
                             for (int l = 0; l < tabla.GetLength(1); l++)
                             {
-                                suma += tabla[i, j];
+                                suma += tabla[k, l];
                             }
                         }
-                        res = suma / 48;
+                        res = suma / tabla.Length;
+                        //res = suma / (12*4);
                         Console.WriteLine("El resultado de la media es de :" + res);
                         break;
                     case 2:
+                        int notaAlumno = 0;
+                        string nombreBuscar;
+                        Console.WriteLine("Introduzca el nombre que desea a buscar");
+                        nombreBuscar = Console.ReadLine();
+                        for (int ñ = 0; ñ < nombres.Length; ñ++)
+                        {
+                            for (int p = 0; p < tabla.GetLength(0); p++)
+                            {
+                                if (nombres[ñ] == nombreBuscar)
+                                {
 
+                                    notaAlumno += tabla[ñ, p];
+
+                                }
+                            }
+                        }
+                        //Console.WriteLine("RES: " + notaAlumno/4);
+                        if (notaAlumno != 0)
+                        {
+                            Console.WriteLine("RES: " + notaAlumno / Enum.GetNames(typeof(asignauras)).Length);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Introduzca el nombre adecuado");
+                        }
                         break;
                     case 3:
+                        int sumaTotalAsignaturas = 0;
+                        string asignaturaBuscar;
+                        Console.WriteLine("Introduzca una asignatura a buscar");
+                        asignaturaBuscar = Console.ReadLine();
+                        string[] valoresEnum = Enum.GetNames(typeof(asignauras));
+                        for (int o = 0; o < valoresEnum.Length; o++)
+                        {
+                            for (int s = 0; s < tabla.GetLength(1); s++)
+                            {
+                                if (asignaturaBuscar == valoresEnum[o])
+                                {
+                                    sumaTotalAsignaturas += tabla[o, s];
+                                }
+                            }
+                        }
+                        if (sumaTotalAsignaturas != 0)
+                        {
+                            Console.WriteLine("La media de la asignatura es " + sumaTotalAsignaturas / tabla.GetLength(0));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Intrduzca una asignatura valida");
+                        }
 
                         break;
                     case 4:
+                        //notas de un alumno determinado
+                        bool verificar = false;
+                        string nombreBuscar1;
+                        Console.WriteLine("Introduzca el nombre que desea a buscar");
+                        nombreBuscar1 = Console.ReadLine();
+                        Console.WriteLine("\nNOTAS");
 
+                        for (int ñ = 0; ñ < nombres.Length; ñ++)
+                        {
+                            for (int t = 0; t < tabla.GetLength(0); t++)
+                            {
+                                if (nombres[ñ] == nombreBuscar1)
+                                {
+                                    Console.WriteLine(tabla[t, ñ]);
+                                    verificar = true;
+                                }
+                            }
+
+                        }
+                        if (verificar == false)
+                        {
+                            Console.WriteLine("Introduzca un nombre correcto");
+                        }
                         break;
                     case 5:
+                        int nMax = 0;
+                        int nMin = 10;
+                        bool verificar1 = false;
+                        string nombreBuscar2 = "";
+                        Console.WriteLine("Introduzca el nombre que desea a buscar");
+                        nombreBuscar2 = Console.ReadLine();
+                        Console.WriteLine("\nNOTAS");
 
+                        for (int ñ = 0; ñ < nombres.Length; ñ++)
+                        {
+                            for (int t = 0; t < tabla.GetLength(0); t++)
+                            {
+                                if (nombres[ñ] == nombreBuscar2)
+                                {
+                                    int n = tabla[t, ñ];
+                                    if (nMax < n)
+                                    {
+                                        nMax = n;
+                                        verificar1 = true;
+                                    }
+                                    else if (nMin > n)
+                                    {
+                                        nMin = n;
+                                    }
+                                    //else if(nMin>n)
+                                }
+                            }
+
+                        }
+                        if (verificar1 == false)
+                        {
+                            Console.WriteLine("Introduzca un nombre correcto");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La nota máxima de " + nombreBuscar2 + " es un " + nMax + " y la mínima un " + nMin);
+                        }
                         break;
                     case 6:
-
+                        for (int v = 0; v < tabla.GetLength(0); v++)
+                        {
+                            for (int w = 0; w < tabla.GetLength(1); w++)
+                            {
+                                Console.WriteLine(tabla[v, w]);
+                            }
+                        }
                         break;
                     case 7:
 
