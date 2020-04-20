d74e6dfaaacd7ebbc015dcb9356d73fc32ee93ae
pelea08 <pelea08@users.noreply.github.com>
2020-04-21 00:55:35 +0200

    Ejer3 Servicios Casi Finalizado

---
+++ ActividadesCuarentena/Servicios/Ejer3Ev1/Ejer3Ev1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Ejer3Ev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDirectorio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubDirectorio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtExtensión = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtDirectorio
+            // 
+            this.txtDirectorio.Location = new System.Drawing.Point(32, 73);
+            this.txtDirectorio.Name = "txtDirectorio";
+            this.txtDirectorio.Size = new System.Drawing.Size(141, 22);
+            this.txtDirectorio.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Directorio";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cambiar Directorio ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Subdirectorios";
+            // 
+            // txtSubDirectorio
+            // 
+            this.txtSubDirectorio.Location = new System.Drawing.Point(216, 73);
+            this.txtSubDirectorio.Multiline = true;
+            this.txtSubDirectorio.Name = "txtSubDirectorio";
+            this.txtSubDirectorio.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSubDirectorio.Size = new System.Drawing.Size(141, 105);
+            this.txtSubDirectorio.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Archivos";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(426, 73);
+            this.txtArchivo.Multiline = true;
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtArchivo.Size = new System.Drawing.Size(141, 105);
+            this.txtArchivo.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(670, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Extensión";
+            // 
+            // txtExtensión
+            // 
+            this.txtExtensión.Location = new System.Drawing.Point(653, 73);
+            this.txtExtensión.Name = "txtExtensión";
+            this.txtExtensión.Size = new System.Drawing.Size(100, 22);
+            this.txtExtensión.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 281);
+            this.Controls.Add(this.txtExtensión);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSubDirectorio);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDirectorio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ejercicio 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDirectorio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubDirectorio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtExtensión;
+    }
+}
+
---
+++ ActividadesCuarentena/Servicios/Ejer3Ev1/Ejer3Ev1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer3Ev1
+{
+    public partial class Form1 : Form
+    {
+        static bool verificar = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            List<string> alm = new List<string>();
+
+            string ruta = txtDirectorio.Text;
+            txtArchivo.Text = "";
+
+            try
+            {
+                Directory.SetCurrentDirectory(ruta);
+                verificar = true;
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Introduzca un valor en directorio");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Introduzca un directorio valido");
+            }
+            if (verificar)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+                foreach (DirectoryInfo directoryInfo1 in directoryInfo.GetDirectories())
+                {
+                    txtSubDirectorio.Text += directoryInfo1 + "\n";
+                }
+                foreach (FileInfo fileInfo in directoryInfo.GetFiles())
+                {
+                    //Si no pone nada mostramos todo
+                    if (txtExtensión.Text == "")
+                    {
+                        txtArchivo.Text += fileInfo + "\n";
+                    }
+                    else
+                    {
+                        string[] almacen = new string[directoryInfo.GetFiles().Length];
+
+                        alm.Add(fileInfo.Name);
+
+                        //Pongo mayor que 1 porque siempre añade el actual pero no lo muestra y me parecio la manera de solventar el problema lo mas rápido
+                        if (alm.Count > 1)
+                        {
+                            for (int i = 0; i < alm.Count; i++)
+                            {
+                                if (alm[i].Split('.')[1] == txtExtensión.Text)//{
+                                    txtArchivo.Text += fileInfo + "\n";
+                            }
+                        }
+                        else
+                        {
+
+                            MessageBox.Show("Introuzca una extension valida");
+                        }
+
+                    }
+
+                }
+
+
+
+            }
+        }
+    }
+}
+
