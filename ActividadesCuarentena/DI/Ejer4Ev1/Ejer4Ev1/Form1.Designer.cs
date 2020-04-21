4acd7dbe50170cbec6f4cb4a2544fc8050e80c9e
pelea08 <pelea08@users.noreply.github.com>
2020-04-22 00:01:34 +0200

    Ejercicio 4 DI Finalizado

---
+++ ActividadesCuarentena/DI/Ejer4Ev1/Ejer4Ev1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Ejer4Ev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 179);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "IZQUIERDA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cordenadas);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(263, 179);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DERECHA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cordenadas);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 228);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ActividadesCuarentena/DI/Ejer4Ev1/Ejer4Ev1/Form1.cs
@@ -0,0 +1,116 @@
+﻿#define TECLAS
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer4Ev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void cordenadas(object sender, MouseEventArgs e)
+        {
+
+            if (sender is Button)
+            {
+                int cordenadaX = ((Button)sender).Location.X;
+                int cordenadaY = ((Button)sender).Location.Y;
+
+                this.Text = "Mouse Tester X:" + (cordenadaX + e.X) + " Y: " + (cordenadaY + e.Y);
+            }
+            else
+            {
+                this.Text = "Mouse Tester X:" + e.X + " Y: " + e.Y;
+            }
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cordenadas(sender, e);
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Text = "Mouse Tester";
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                button1.BackColor = Color.Transparent;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                button2.BackColor = Color.Transparent;
+            }
+            else
+            {
+                button2.BackColor = Color.Transparent;
+                button1.BackColor = Color.Transparent;
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                button1.BackColor = Color.Red;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                button2.BackColor = Color.Blue;
+            }
+            else
+            {
+                button2.BackColor = Color.Blue;
+                button1.BackColor = Color.Red;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Desea salir?", "Mouse Tester", MessageBoxButtons.OKCancel, MessageBoxIcon.Error) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+#if TECLA
+        //Hago lo de poner keypress vacio para que al ejecutar no me dea problemas 
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        { }
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Text = "Mouse Tester";
+            }
+            else
+            {
+                this.Text = e.KeyCode.ToString();
+            }
+        }
+#else
+        //AQUI ABAJO IGUAL QUE ARRIBA PARA EVITAR ERRORES Y TENER QUE ANDAR A AÑADIRLO
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        { }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                this.Text = "Mouse Tester";
+            }
+            else
+            {
+                this.Text = e.KeyChar.ToString();
+            }
+        }
+#endif
+    }
+}
