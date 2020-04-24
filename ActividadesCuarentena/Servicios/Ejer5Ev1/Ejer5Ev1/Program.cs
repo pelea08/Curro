e49e5f2fa409df5d0f708da9475f718e35f5c320
pelea08 <pelea08@users.noreply.github.com>
2020-04-24 10:30:49 +0200

    Actualización Ejer5 Servicios

---
+++ ActividadesCuarentena/Servicios/Ejer5Ev1/Ejer5Ev1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejer5Ev1
+{
+    class Program
+    {
+        static readonly object l = new object();
+        static bool finalizacion = true;
+        static int stock = 100;
+        static void Main(string[] args)
+        {
+            string[] nombres = { "Halcón Milenario", "Nostromo", "Discovery 1", "Lego Spaceship", " Gatobús" };
+
+
+
+            Thread gestionAlmacen = new Thread(gestorAlmacen);
+            gestionAlmacen.Start();
+
+            Thread[] conjuntoNave = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                conjuntoNave[i] = new Thread(nave);
+            }
+            for (int j = 0; j < conjuntoNave.Length; j++)
+            {
+                conjuntoNave[j].Start(nombres[j]);
+            }
+
+
+            Console.ReadKey();
+
+            finalizacion = false;
+            Console.SetCursorPosition(1, 20);
+            Console.WriteLine("El ultimo valor del stock es " + stock);
+            Console.ReadKey();
+        }
+        static void gestorAlmacen()
+        {
+            lock (l)
+            {
+                Monitor.Wait(l);
+                stock += 100;
+                Console.SetCursorPosition(1, 20);
+                Console.WriteLine("--------> Se ha aumentado el stock en 100 unidades, en total hay" + stock + " <--------");
+            }
+        }
+        static void nave(object nombre)
+        {
+            Random r = new Random();
+            int aleatorio = r.Next(0, 3);
+            while (finalizacion)
+            {
+                lock (l)
+                {
+                    if (stock <= 0)
+                    {
+                        Console.SetCursorPosition(1, 15);
+                        Console.WriteLine((string)nombre + " no puede cargar por falta de stock avisa y se va");
+                        Monitor.Pulse(l);
+                    }
+                    if (aleatorio == 0)
+                    {
+                        stock -= 50;
+                        Console.SetCursorPosition(1, 5);
+                        Console.WriteLine((string)nombre + " ha quitado 50 unidades, quedan" + stock + " en el almacén");
+                    }
+                    else
+                    {
+                        stock += 50;
+                        Console.SetCursorPosition(1, 10);
+                        Console.WriteLine((string)nombre + " ha  agregado 50 unidades, quedan" + stock + " en el almacén");
+                    }
+                    if (stock < 1)
+                    {
+                        Monitor.Pulse(l);
+                    }
+                }
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ ActividadesCuarentena/Servicios/Ejer5Ev1/Ejer5Ev1B/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5Ev1B
+{
+    class Program
+    {
+        public delegate void Proceso(int a);
+
+        static void Main(string[] args)
+        {
+
+            int op = 0;
+
+            Proceso opp = new Proceso(listaProcesos);
+            do
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("1-.Listado de procesos");
+                Console.WriteLine("2-.DLLs de un proceso");
+                Console.WriteLine("3-.Cierre de un proceso");
+                Console.WriteLine("4.-Salir");
+                op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        try
+                        {
+                            int prioridad;
+                            Console.WriteLine("Introduce una prioridad");
+                            prioridad = Convert.ToInt32(Console.ReadLine());
+                            opp(prioridad);
+
+
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Introduce números");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Introduce un ´numero adecuado");
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            int pid;
+                            Console.WriteLine("Introduce un pid");
+                            pid = Convert.ToInt32(Console.ReadLine());
+                            dlls(pid);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Introduce números");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Introduce un ´numero adecuado");
+                        }
+
+                        break;
+                    case 3:
+                        try
+                        {
+                            int pid;
+                            Console.WriteLine("Introduce un pid");
+                            pid = Convert.ToInt32(Console.ReadLine());
+                            Process p = Process.GetProcessById(pid);
+                            p.Kill();
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Introduce números");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Introduce un ´numero adecuado");
+                        }
+                        break;
+
+
+
+
+
+
+                }
+            } while (op != 4);
+        }
+        static void listaProcesos(int prioridad)
+        {
+            Process[] p = Process.GetProcesses();
+            foreach (Process process in p)
+            {
+                if (process.BasePriority <= prioridad)
+                {
+                    Console.WriteLine("PID: " + process.Id + " Nombre: " + process.ProcessName + " Prioridad: " + process.BasePriority);
+                }
+            }
+
+
+
+        }
+        static void dlls(int pid)
+        {
+            try
+            {
+                Process pp = Process.GetProcessById(pid);
+                ProcessModuleCollection pepe = pp.Modules;
+                foreach (ProcessModule processModule in pepe)
+                {
+                    Console.WriteLine(processModule.ModuleName);
+                }
+            }
+            catch (ArgumentException)
+            {
+
+            }
+        }
+        static void cierreProceso(int id)
+        {
+            Process p = Process.GetProcessById(id);
+            p.CloseMainWindow();
+
+        }
+    }
+}
